24263a2e3e4f6b622127f011bf8cdb67a89656fd
Vivek Prakash <prakashvivek444@gmail.com>
2020-08-06 20:46:40 +0530

    Added local project files

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IconComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
